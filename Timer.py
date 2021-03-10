5bb9f35299375877ddf1d70b9bbbaffb00bbc622
ZackJacobs <79279288+ZackJacobs@users.noreply.github.com>
2021-03-10 11:42:07 -0500

    Update Timer.py

--- Timer.py
+++ Timer.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
-import time
+import time as time
 
 # starts timer and returns the system time at which the timer started
 def beginTime():
-  return time.time()
+  return(time.time())
 
 #takes the time started as a parameter and returns the amount of time elapsed since the timer started
 def finishTime(startTime):
   endTime = time.time
   elapsedTime = endTime - startTime
-  return elapsedTime
+  return(elapsedTime)
 
 #takes elapsed time as a parameter and returns it in an hours:mins:secs format
 def formatTime(elapsedTime):
@@ -17,7 +17,7 @@ def formatTime(elapsedTime):
   sec = elapsedTime % 60
   hours = mins // 60
   mins = mins % 60
-  return ("\nTime Lapsed = {0}:{1}:{2}\n".format(int(hours),int(mins),sec))
+  return("\nTime Lapsed = {0}:{1}:{2}\n".format(int(hours),int(mins),sec))
 
 #takes elapsed time and name of worker as parameters and returns it in an hours:mins:secs format with the name of the worker
 def formatTimeWithWorker(elapsedTime, worker):
@@ -25,4 +25,4 @@ def formatTimeWithWorker(elapsedTime, worker):
   sec = elapsedTime % 60
   hours = mins // 60
   mins = mins % 60
-  return ( "\nWorker: " + worker + "\nTime Lapsed = {0}:{1}:{2}\n".format(int(hours),int(mins),sec))
+  return("\nWorker: " + worker + "\nTime Lapsed = {0}:{1}:{2}\n".format(int(hours),int(mins),sec))
