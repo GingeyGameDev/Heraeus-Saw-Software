d29f67aa49e54f1dcd2fa7bb69188cc47e22ae9c
ZackJacobs <zackjacobs2022@gmail.com>
2021-03-11 13:19:48 -0500

    added comments

--- scripts/VibrationSensor.py
+++ scripts/VibrationSensor.py
@@ -6,12 +6,14 @@ channel = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
+#prints "movement detected" when run
 def callback(channel):
     if(GPIO.input(channel)):
         print("Movement detected")
     else:
         print("Movement detected")
     
+#gives out a signal dependant on whether the pin goes high or low and runs the function if it is high
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime = 300)
 GPIO.add_event_callback(channel, callback)
 
