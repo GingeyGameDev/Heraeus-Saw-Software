794a1c5251c8bfa36046d4e48038c591f7212fa2
ZackJacobs <zackjacobs2022@gmail.com>
2021-03-11 12:34:06 -0500

    added vibration sensor script

---
+++ scripts/VibrationSensor.py
@@ -0,0 +1,18 @@
+import RFi.GPIO as GPIO
+from import MotionSensor
+
+channel = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+def callback(channel):
+    if(GPIO.input(channel)):
+        print("Movement detected")
+    else:
+        print("Movement detected")
+    
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime = 300)
+GPIO.add_event_callback(channel, callback)
+
+while(True):
+    time.sleep(1)
