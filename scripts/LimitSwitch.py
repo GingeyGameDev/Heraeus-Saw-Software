bd18cc2bed9cb9e03355025da534ae31af1f2341
EpicHeraeus <epicheraeus@gmail.com>
2021-03-18 13:56:49 -0400

    FIxed limit switch code

--- scripts/LimitSwitch.py
+++ scripts/LimitSwitch.py
@@ -1,29 +1,9 @@
-#!/usr/bin/python
-#import gpiozero as gpiozero
-#import time as time
-#import Timer as Timer
-
-#Makes a limit switch variable taking an input from the pin the button is on
-#limitSwitch = gpiozero.Button(11)
-
-#tests every .5 seconds if the switch is being held down, and if it isn't, it will start a timer which will end once the button is pressed down again
-"""while True:
-  if not limitSwitch.is_held:
-    startTime = Timer.beginTime()
-    print("switch has been let go")
-    while not limitSwitch.is_held:
-      time.sleep(.5)
-    else:
-      time.sleep(.5)
-    time.sleep(.5)
-  time.sleep(.5)
-  """
-
 import RPi.GPIO as GPIO
 import time
+import Timer
 
 # Button pin
-P_BUTTON = 11 # adapt to your wiring
+P_BUTTON = 7 # adapt to your wiring
 
 def setup():
     GPIO.setmode(GPIO.BOARD)
