95f70ceab7b94a4d9711086113bbd9bf07fe90d6
GingeyGameDev <mattvaughngames@gmail.com>
2021-03-19 10:26:36 -0400

    added test pattern to limitswitch

--- scripts/LimitSwitch.py
+++ scripts/LimitSwitch.py
@@ -1,6 +1,23 @@
-import RPi.GPIO as GPIO
 import time
 import Timer
+
+
+#pattern for switching between RPis
+try:
+    import sys
+    import RPi
+    sys.modules['RPi'] = RPi
+    sys.modules['RPi.GPIO'] = RPi.GPIO
+
+    import RPi.GPIO as GPIO
+    
+except (RuntimeError, ModuleNotFoundError):
+    import sys
+    import fake_rpi
+
+    sys.modules['RPi'] = fake_rpi.RPi     # Fake RPi
+    sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO # Fake GPIO
+    import RPi.GPIO as GPIO
 
 # Button pin
 P_BUTTON = 7 # adapt to your wiring
