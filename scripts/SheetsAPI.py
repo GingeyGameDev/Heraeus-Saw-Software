2ec0447e4bcb464ab72750392893891f0ae3a6c4
GingeyGameDev <mattvaughngames@gmail.com>
2021-03-23 09:20:46 -0400

    sheets xl api

--- scripts/SheetsAPI.py
+++ scripts/SheetsAPI.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 import gspread as gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import xlwt as xlwt
+from xlutils.copy import copy
+import xlwt
+import xlrd as xlrd
 
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
@@ -13,7 +15,20 @@ def SheetUpdate(sheetName, values):
     sheet = client.open(sheetName)
     sheetInstance = sheet.get_worksheet(0)
     sheetInstance.append_row(values)
+    libreUpdate(sheetName, values)
 
-def libreUpdate():
-    wb = xlwt.open_workbook
-    ws = wb.append_row()
+
+
+def libreUpdate(sheetName, values):
+    rb =  xlrd.open_workbook("scripts/TestSheet.xls")
+
+    wb = copy(rb)
+    sheet = wb.get_sheet(0)
+
+    sheet.write(1,1, "test")
+    sheet.write(5, 5, 'TestSheet 2')
+    sheet.write_merge(3, 4, 3, 4, 'TestSheet 3'
+
+    wb.save('scripts/TestSheet.xls')
+
+libreUpdate('TestSheet', 1) 
