cda026ec4ce705f67997a0f74c1f16194687f715
GPIOctapus <sdj2016@gmail.com>
2021-04-13 14:15:37 -0400

    added csvUpdate()

--- scripts/SheetsAPI.py
+++ scripts/SheetsAPI.py
@@ -24,6 +24,7 @@ def getName():
 
 def libreUpdate(values):
 
+    #tries to open most recent xls file, if not it goes to Template.xls
     date = datetime.date.today()
     try:
         rb =  xlrd.open_workbook("/home/pi/Desktop/XLS Archive/" + str(date.month) + str(date.year) +".xls")    
@@ -41,10 +42,49 @@ def libreUpdate(values):
     for ii in range(0, 10):
         for j in range(0,len(values)):
             sheet.write(emptyRow + ii, j, values[j])
-  
+    
+    #saves copy to archive
     try:
         wb.save("/home/pi/Desktop/XLS Archive/" + str(date.month) + str(date.year) +".xls")
     except(RuntimeError, FileNotFoundError):
         wb.save("scripts/sheets/" + str(date.month) + str(date.year) + " - TEST" + ".xls")
+
+def csvUpdate():
+
+    #wipes csv
+    wipe = open('scripts/webpage/data.csv', 'w')
+    wipe.write("")
+
+    #opens up xls doc like in libreUpdate()
+    date = datetime.date.today()
+    try:
+        rb =  xlrd.open_workbook("/home/pi/Desktop/XLS Archive/" + str(date.month) + str(date.year) +".xls")    
+    except(RuntimeError, FileNotFoundError):
+        rb = xlrd.open_workbook("scripts/sheets/Template.xls")
+
+    sheet = rb.sheet_by_index(0)
+
+
+
+    csv = open('scripts/webpage/data.csv', 'a')
+    i = 0
+    try:
+        while True:
+            currentRow = sheet.row_values(i) #grab current xls row
+        
+            #writes each value into the csv file
+            for values in currentRow:
+                
+                csv.write(str(values))
+                #ensures a line break is placed at the end of each row
+                if values == (len(currentRow)):
+                    csv.write("\n")
+                    break
+                else:
+                    csv.write(",")
+            i = i + 1
+    except IndexError:
+        pass
+
 
 libreUpdate([1,2,3,4,5,6,7,8,9,10])
